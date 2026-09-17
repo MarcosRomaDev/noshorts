fd8cf415c55246f26e814c4590745b0bcfaa7796
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-09-17 22:01:16 +0200

    feat: ocultar Shorts en portada, sidebar y búsquedas

--- content.js
+++ content.js
@@ -1,16 +1,26 @@
 function hideShortsGuideEntry() {
   const guideEntries = document.querySelectorAll("ytd-guide-entry-renderer");
+  const searchGridShelves = document.querySelectorAll("grid-shelf-view-model");
 
   guideEntries.forEach((entry) => {
     if (entry.textContent.includes("Shorts")) {
       entry.style.setProperty("display", "none", "important");
     }
   });
+
+  searchGridShelves.forEach((entry) => {
+    if (entry.textContent.includes("shorts")) {
+      entry.style.setProperty("display", "none", "important");
+    }
+  });
 }
 
+// Oculta las entradas y secciones de Shorts que ya existen en la página.
 hideShortsGuideEntry();
 
+// YouTube añade contenido dinámicamente porque funciona como una SPA.
 const observer = new MutationObserver(hideShortsGuideEntry);
+
 observer.observe(document.documentElement, {
   childList: true,
   subtree: true,
